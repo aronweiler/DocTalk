decd2d885eebf7f67c437ff2a9f3ac5061fc76f0
Aron Weiler <aron.j.weiler@medtronic.com>
2023-06-20 17:42:53 -0700

    Added local llm with langchain using ConversationalRetrievalChain

--- src/run.py
+++ src/run.py
@@ -1,13 +1,19 @@
 import run_local_llm
 import run_openai_llm
+import run_local_llm_langchain
 
 print("Hello!  Answer some questions to get started.\n")
 
 verbose = input("Should we use verbose output (intermediate steps and such)?  Y/N:").upper()
 
 if input("Run Local LLM: (Y/N)").upper() == "Y":
-    top_k = int(input("LOCAL- Select the top_k for search results (how many loaded document results will be used to feed the LLM): "))
-    pre_parse_queries = input("LOCAL- Should we pre-parse search queries?  (This could yield different/better results when searching for documents to feed to the LLM)  Y/N:").upper()
-    run_local_llm.main(top_k, pre_parse_queries == "Y", verbose == "Y")
+    top_k = int(input("LOCAL- Select the top_k for search results (how many loaded document results will be used to feed the LLM): "))    
+    use_langchain = input("Use LangChain? (Y/N)").upper()
+    
+    if use_langchain:
+        run_local_llm_langchain.main(top_k, verbose == "Y")
+    else:
+        pre_parse_queries = input("LOCAL- Should we pre-parse search queries?  (This could yield different/better results when searching for documents to feed to the LLM)  Y/N:").upper()
+        run_local_llm.main(top_k, pre_parse_queries == "Y", verbose == "Y")
 else:
     run_openai_llm.main(verbose == "Y")

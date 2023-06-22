b5c5b4b5eb8f7e792161b89db6722897a79c3038
aronweiler <aronweiler@gmail.com>
2023-06-21 22:57:32 -0700

    Moved to ConversationTokenBufferMemory for memory

--- src/run_llm_langchain.py
+++ src/run_llm_langchain.py
@@ -2,18 +2,19 @@ import os
 import time
 import utilities.console_text as console_text
 import documents
+import shared
 import utilities.calculate_timing as calculate_timing
-
+import callback_handlers
 from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationTokenBufferMemory
 
-def main(llm, embeddings, database_name:str, top_k = 4, search_distance = .5, verbose = False):  
+def main(llm, embeddings, database_name:str, top_k = 4, search_distance = .5, verbose = False, max_tokens = shared.MAX_LOCAL_CONTEXT_SIZE):  
     db = documents.get_database(embeddings, database_name) 
     
-    vectordbkwargs = {"search_distance": search_distance, "k": top_k}
+    vectordbkwargs = {"search_distance": search_distance, "k": top_k, "search_type": "similarity"}
 
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, input_key="question", output_key="answer")
-    qa = ConversationalRetrievalChain.from_llm(llm, db.as_retriever(search_kwargs=vectordbkwargs), memory=memory, verbose=verbose, return_source_documents=True)
+    memory = ConversationTokenBufferMemory(llm=llm, max_token_limit=max_tokens, memory_key="chat_history", return_messages=True, input_key="question", output_key="answer")    
+    qa = ConversationalRetrievalChain.from_llm(llm, db.as_retriever(search_kwargs=vectordbkwargs), memory=memory, verbose=verbose, return_source_documents=True, callbacks=[callback_handlers.DebugCallbackHandler()])
 
     while True:        
         query = input("Query (x to exit): ")

c310f0664c0a7f81988a21d33778d2becf40849b
aronweiler <aronweiler@gmail.com>
2023-06-24 17:58:37 -0700

    Using memory on vector store tool

--- src/tools/vector_store_tool.py
+++ src/tools/vector_store_tool.py
@@ -1,14 +1,14 @@
+import time
 import os
 import shared
-import prompts
 from document_loader import get_database
 from selector import get_llm, get_embedding
 from langchain.chains import RetrievalQA
-from langchain.prompts import PromptTemplate
+import utilities.calculate_timing as calculate_timing
 
 class VectorStoreTool:
 
-    def __init__(self, database_name, local, top_k = 4, search_type = "mmr", search_distance = .5, verbose = False, max_tokens = shared.MAX_LOCAL_CONTEXT_SIZE, return_source_documents = False):
+    def __init__(self, memory, database_name, run_locally, top_k = 4, search_type = "mmr", search_distance = .5, verbose = False, max_tokens = shared.MAX_LOCAL_CONTEXT_SIZE, return_source_documents = False, return_direct = None):
             self.database_name = database_name
             self.top_k = top_k
             self.search_type = search_type
@@ -18,31 +18,33 @@ class VectorStoreTool:
             self.return_source_documents = return_source_documents
 
             # Load the specified database using non-local embeddings
-            db = get_database(get_embedding(local), database_name)    
+            db = get_database(get_embedding(run_locally), database_name)    
 
             vectordbkwargs = {"search_distance": search_distance, "k": top_k, "search_type": search_type}
 
             # Get the open ai llm
-            llm = get_llm(local)
+            llm = get_llm(run_locally)
             
             chain_type_kwargs = {}
 
-            # if local:
-            #      prompt = PromptTemplate(template=prompts.OPEN_LLAMA_INSTRUCT_PROMPT_TMPL, input_variables=["context", "question"])
-            #      chain_type_kwargs = {"prompt": prompt}
-
-            self.retrieval_qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=db.as_retriever(search_kwargs=vectordbkwargs), verbose=verbose, return_source_documents=return_source_documents, chain_type_kwargs=chain_type_kwargs)           
+            self.retrieval_qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=db.as_retriever(search_kwargs=vectordbkwargs), memory=memory, verbose=verbose, return_source_documents=return_source_documents, chain_type_kwargs=chain_type_kwargs)           
 
             print(f"VectorStoreTool initialized with database_name={database_name}, top_k={top_k}, search_type={search_type}, search_distance={search_distance}, verbose={verbose}, max_tokens={max_tokens}")
 
     def run(self, query:str) -> str:
-        print("VectorStoreTool got: ", query)
-        # query = f"Summarize information about: {query}"
-        # print("VectorStoreTool executing: ", query)
+        print(f"\nVectorStoreTool got: {query}")
+
+        start_time = time.time()
+        result = self.retrieval_qa({"query": query})
+        end_time = time.time()
+      
+        elapsed_time = end_time - start_time
+        print("Operation took: ", calculate_timing.convert_milliseconds_to_english(elapsed_time * 1000))
 
         result = self.retrieval_qa({"query": query})
         result_string = result['result']
 
+        # When this tool is used from the self_ask_agent_tool, it doesn't 
         if self.return_source_documents:
             # Append the source docs to the result, since we can't return anything else but a string??  langchain... more like lamechain, amirite??
             source_docs_list = [f"\t- {os.path.basename(d.metadata['source'])} page {int(d.metadata['page']) + 1}" if 'page' in d.metadata else f"\t- {os.path.basename(d.metadata['source'])}" for d in result["source_documents"]]

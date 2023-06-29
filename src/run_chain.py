73936ca9da51b118b5dee49c342d1b89bc3eeb37
aronweiler <aronweiler@gmail.com>
2023-06-29 14:49:40 -0700

    Changes to allow getting chain, memory, db with different args

--- src/run_chain.py
+++ src/run_chain.py
@@ -13,13 +13,27 @@ from selector import get_llm, get_embedding
 import argparse
 import shared
 
-def main(llm, embeddings, database_name:str, top_k = 4, search_type = "mmr", search_distance = .5, verbose = False, max_tokens = shared.MAX_LOCAL_CONTEXT_SIZE):  
+def main(llm, embeddings, database_name:str, top_k = 4, chain_type = "stuff", search_type = "mmr", search_distance = .5, verbose = False, max_tokens = shared.MAX_LOCAL_CONTEXT_SIZE):  
     db = get_database(embeddings, database_name) 
-    
-    vectordbkwargs = {"search_distance": search_distance, "k": top_k, "search_type": search_type}
+    memory = get_memory(llm=llm, max_tokens=max_tokens)
 
+    qa = get_chain(llm, memory, db, top_k, chain_type, search_type, search_distance, verbose)
+
+    run_chain(qa)
+
+def get_chain(llm, memory, db, top_k, chain_type, search_type, search_distance, verbose):
+    vectordbkwargs = {"search_distance": search_distance, "k": top_k, "search_type": search_type}
+    
+    qa = ConversationalRetrievalChain.from_llm(llm, db.as_retriever(search_kwargs=vectordbkwargs), chain_type=chain_type, memory=memory, verbose=verbose, return_source_documents=True, callbacks=[callback_handlers.DebugCallbackHandler()])
+
+    return qa
+
+def get_memory(llm, max_tokens):
     memory = ConversationTokenBufferMemory(llm=llm, max_token_limit=max_tokens, memory_key="chat_history", return_messages=True, input_key="question", output_key="answer")    
-    qa = ConversationalRetrievalChain.from_llm(llm, db.as_retriever(search_kwargs=vectordbkwargs), memory=memory, verbose=verbose, return_source_documents=True, callbacks=[callback_handlers.DebugCallbackHandler()])
+    
+    return memory
+    
+def run_chain(qa):      
 
     while True:        
         query = input("Query (x to exit): ")
@@ -44,7 +58,7 @@ def main(llm, embeddings, database_name:str, top_k = 4, search_type = "mmr", sea
 
         print("Operation took: ", calculate_timing.convert_milliseconds_to_english(elapsed_time * 1000))
 
-def run(run_local:bool, database_name:str, verbose:bool, top_k:int, search_type:str, search_distance:float):    
+def run(run_local:bool, database_name:str, verbose:bool, top_k:int, search_type:str, search_distance:float, chain_type:str):    
     print("-------- Running LLM --------")
     print("run_local:", run_local)
     print("database_name:", database_name)
@@ -52,6 +66,7 @@ def run(run_local:bool, database_name:str, verbose:bool, top_k:int, search_type:
     print("top_k:", top_k)
     print("search_type: ", search_type)
     print("search_distance:", search_distance)
+    print("chain_type:", chain_type)
     print("-----------------------------")
     
     if run_local:    
@@ -63,20 +78,23 @@ def run(run_local:bool, database_name:str, verbose:bool, top_k:int, search_type:
         embeddings = get_embedding(False)
         max_tokens = shared.MAX_OPEN_AI_CONTEXT_SIZE
         
-    main(llm, embeddings, database_name, top_k, search_type, search_distance, verbose, max_tokens=max_tokens)
+    main(llm, embeddings, database_name, top_k, chain_type, search_type, search_distance, verbose, max_tokens=max_tokens)
       
-parser = argparse.ArgumentParser()
+if __name__ == "__main__":
 
-# Add arguments
-parser.add_argument('--run_open_ai', action='store_true', default=False, help='Use OpenAI vs. local LLM')
-parser.add_argument('--database_name', type=str, default="default", help='Database name to use for document storage')
-parser.add_argument('--verbose', action='store_true', default=False, help='Verbose mode')
-parser.add_argument('--top_k', type=int, default=5, help='Top K value- number of documents or chunks of documents for the LLM to use to answer a question (Note: this can kill performance locally)')
-parser.add_argument('--search_distance', type=float, default=0.1, help='Search distance limits the similarity search in the vector database (value should be 0 and 1, lower value indicates a wider search)')
-parser.add_argument('--search_type', type=str, default='mmr', help='Search type can be either "similarity", or "mmr". Default is "mmr"')
+    parser = argparse.ArgumentParser()
 
-# Parse the command-line arguments
-args = parser.parse_args()
+    # Add arguments
+    parser.add_argument('--run_open_ai', action='store_true', default=False, help='Use OpenAI vs. local LLM')
+    parser.add_argument('--database_name', type=str, default="default", help='Database name to use for document storage')
+    parser.add_argument('--verbose', action='store_true', default=False, help='Verbose mode')
+    parser.add_argument('--top_k', type=int, default=5, help='Top K value- number of documents or chunks of documents for the LLM to use to answer a question (Note: this can kill performance locally)')
+    parser.add_argument('--search_distance', type=float, default=0.1, help='Search distance limits the similarity search in the vector database (value should be 0 and 1, lower value indicates a wider search)')
+    parser.add_argument('--search_type', type=str, default='mmr', help='Search type can be either "similarity", or "mmr". Default is "mmr"')
+    parser.add_argument('--chain_type', type=str, default='stuff', help='Chain type supported by langchain')
 
-# Call the run() method with parsed arguments
-run(run_local=args.run_open_ai == False, database_name=args.database_name, verbose=args.verbose, top_k=args.top_k, search_type=args.search_type, search_distance=args.search_distance)
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Call the run() method with parsed arguments
+    run(run_local=args.run_open_ai == False, database_name=args.database_name, verbose=args.verbose, top_k=args.top_k, search_type=args.search_type, chain_type=args.chain_type, search_distance=args.search_distance)    

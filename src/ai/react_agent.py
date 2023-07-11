267dae6b66abce260d5958683ebf2307c7ebd789
aronweiler <aronweiler@gmail.com>
2023-07-10 18:18:37 -0700

    Added react agent to ais

--- src/ai/react_agent.py
+++ src/ai/react_agent.py
@@ -1,83 +1,39 @@
-# Moved this here from src where it was being run stand-alone
-# TODO: Refactor to be an AbstractAI and use with a runner
-
-## Run a vector store agent over multiple vector stores
-## The intent here would be to use OpenAI's LLM to route requests to the proper chain
-## and then have the chains associated with the different vector stores use their own LLMs
-## e.g. a publicly available set of documents is used with the OpenAI LLM and private docs use a local LLM
-
-from shared.selector import get_llm, get_embedding
-import argparse
-import shared.constants as constants
-import time
-import utilities.console_text as console_text
-import shared.constants as constants
-import utilities.calculate_timing as calculate_timing
-from langchain.memory import ConversationTokenBufferMemory, ReadOnlySharedMemory
+from shared.selector import get_llm
+from langchain.memory import ConversationTokenBufferMemory
 from langchain.agents import initialize_agent
 from langchain.agents import AgentType
-from tool_loader import load_tools_from_file
 
-def main(router_llm, configuration_file, verbose, max_tokens = constants.MAX_LOCAL_CONTEXT_SIZE):  
-    
-    memory = ConversationTokenBufferMemory(llm=router_llm, max_token_limit=max_tokens, memory_key="chat_history", return_messages=True)   
+from ai.abstract_ai import AbstractAI
+from ai.ai_result import AIResult
 
-    # Share my memory with all my buds
-    tools = load_tools_from_file(configuration_file, memory)
-    
-    agent_chain = initialize_agent(tools, router_llm, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, verbose=verbose, memory=memory)
+from ai.react_agent_configuration import ReActAgentConfiguration
 
-    while True:
-        query = input("Query (x to exit): ")
+from ai.agent_tools.utilities.tool_loader import load_tools
 
-        if query == "x":
-            exit()
+class ReActAgent(AbstractAI):
 
-        if len(memory.buffer) == 0:
-            query = "Without using knowledge prior to this conversation, and using one or more of the tools available to you, respond to the following: " + query             
+    def configure(self, json_args) -> None:        
+        self.configuration = ReActAgentConfiguration(json_args)
 
-        # Time it
-        start_time = time.time()       
+        router_llm = get_llm(self.configuration.run_locally, float(self.configuration.ai_temp))
+        memory = self._get_memory(router_llm, self.configuration.max_tokens) if self.configuration.use_memory else None        
 
-        # Run the query
-        result = agent_chain.run(input=query)
+        tools = load_tools(json_args, memory, None)
 
-        end_time = time.time()
+        self.agent_chain = initialize_agent(tools, router_llm, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory)     
 
-        # print the answer
-        console_text.print_green(result)        
+
+    def _get_memory(self, llm, max_tokens):
+        memory = ConversationTokenBufferMemory(llm=llm, max_token_limit=max_tokens, memory_key="chat_history", return_messages=True)    
         
-        elapsed_time = end_time - start_time
-
-        print("Operation took: ", calculate_timing.convert_milliseconds_to_english(elapsed_time * 1000))
-
-
-
-def run(run_local:bool, verbose:bool, configuration_file:str):    
-    print("-------- Running LLM --------")
-    print("run_local:", run_local)
-    print("verbose:", verbose)
-    print("configuration_file:", configuration_file)
-    print("-----------------------------")
+        return memory
     
-    if run_local:    
-        llm = get_llm(True)
-        max_tokens = constants.MAX_LOCAL_CONTEXT_SIZE
-    else:
-        llm = get_llm(False)
-        max_tokens = constants.MAX_OPEN_AI_CONTEXT_SIZE
-        
-    main(llm, configuration_file, verbose, max_tokens)
-      
-parser = argparse.ArgumentParser()
 
-# Add arguments
-parser.add_argument('--run_open_ai', action='store_true', default=False, help='Use OpenAI vs. local LLM')
-parser.add_argument('--verbose', action='store_true', default=False, help='Verbose mode')
-parser.add_argument('--config', type=str, required=True, help='Path to the tools configuration file')
+    def query(self, input):
 
-# Parse the command-line arguments
-args = parser.parse_args()
+        result = self.agent_chain.run(input=input)
 
-# Call the run() method with parsed arguments
-run(run_local=args.run_open_ai == False, verbose=args.verbose, configuration_file=args.config)
+        ai_results = AIResult(result) 
+
+        return ai_results
+

754098a66febc5b6919564132b1fc04404798e14
aronweiler <aronweiler@gmail.com>
2023-07-14 12:00:07 -0700

    Added support for multiple runners in a config

--- src/run.py
+++ src/run.py
@@ -39,24 +39,12 @@ with open(args.config) as config_file:
 ai_type = config['ai']['type']
 ai_args = config['ai']['arguments']
 
-runner_type = config['runner']['type']
-runner_args = config['runner']['arguments']
-
 print("ai_type: ", ai_type)
 
 # Print out the list of arguments in a nice human readable format: 
 print("ai_args:")
 for key, value in ai_args.items():
     print(f"\t{key}: {value}")
-
-print("runner_type: ", runner_type)
-
-# Print out the list of arguments in a nice human readable format:
-print("runner_args:")
-for key, value in runner_args.items():
-    print(f"\t{key}: {value}")
-
-# start the application in the specified mode for the specified type
 
 # get the ai
 ai_class = AI_TYPES.get(ai_type)
@@ -66,20 +54,39 @@ if ai_class:
 else:
     raise ValueError(f"ai_type is undefined, {ai_type}")
 
-# run the mode
-runner_class = RUNNER_TYPES.get(runner_type)
-if runner_class:    
-    # If there are arguments in the runner config, pass them on
-    if runner_args:
-        runner = runner_class(runner_args)
-    else:
-        runner = runner_class()
-
-    if callable(runner):
-        runner(ai) # This is here because starting FastAPI in proc demands it (see the RestAPIRunner)
-    runner.run(abstract_ai=ai)
+# If the runners node exists, get that.  Otherwise get the runner node, and put it into a list.  If the runner node doesn't exist, throw an error.
+if 'runners' in config:        
+    runners = config['runners']
 else:
-    raise ValueError(f"mode is undefined, {runner_type}")
+    runners = [config]
+        
+if not runners:
+        raise ValueError("No runners defined")
+
+# TODO: Make this multi-threaded
+for runner in runners:
+    runner_type = runner['runner']['type']
+    runner_args = runner['runner']['arguments']
+    print("runner_type: ", runner_type)
+
+    # Print out the list of arguments in a nice human readable format:
+    print("runner_args:")
+    for key, value in runner_args.items():
+        print(f"\t{key}: {value}")
+
+    runner_class = RUNNER_TYPES.get(runner_type)
+    if runner_class:    
+        # If there are arguments in the runner config, pass them on
+        if runner_args:
+            runner = runner_class(runner_args)
+        else:
+            runner = runner_class()
+
+        if callable(runner):
+            runner(ai) # This is here because starting FastAPI in proc demands it (see the RestAPIRunner)
+        runner.run(abstract_ai=ai)
+    else:
+        raise ValueError(f"runner type is undefined, {runner_type}")
 
 
 # testing

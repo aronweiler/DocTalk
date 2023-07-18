fcca1f057226a5018d8fd525f74ab1650b254349
aronweiler <aronweiler@gmail.com>
2023-07-18 16:31:13 -0700

    Updated test c# code

--- src/runners/unit_tests/examples/c_sharp_code.cs
+++ src/runners/unit_tests/examples/c_sharp_code.cs
@@ -6,16 +6,16 @@ namespace Manufacturing.Shared.Core
     {
         public bool Success { get; }
 
-        public string Message { get; protected set; }
+        public string Message { get; set; }
 
-        public Exception Exception { get; protected set; }
+        public Exception Exception { get; set; }
 
-        protected Response(bool success)
+        public Response(bool success)
         {
             Success = success;
         }
 
-        protected Response(bool success, string message)
+        public Response(bool success, string message)
         {
             Success = success;
             Message = message;
@@ -39,6 +39,42 @@ namespace Manufacturing.Shared.Core
         public static Response Succeeded(string message)
         {
             return new Response(true, message);
+        }
+    }
+
+    public class Response<T> : Response
+    {
+        public T Value { get; private set; }
+
+        private Response(bool success, T value)
+            : base(success)
+        {
+            Value = value;
+        }
+
+        private Response(bool success, T value, string message) : base(success, message)
+        {
+            Value = value;
+        }
+
+        public new static Response<T> Failed(string message)
+        {
+            return Failed(message, null);
+        }
+
+        public new static Response<T> Failed(string message, Exception exception)
+        {
+            return new Response<T>(false, default(T)) { Message = message, Exception = exception };
+        }
+
+        public static Response<T> Succeeded(T value)
+        {
+            return new Response<T>(true, value);
+        }
+
+        public static Response<T> Succeeded(T value, string message)
+        {
+            return new Response<T>(true, value, message);
         }
     }
 }

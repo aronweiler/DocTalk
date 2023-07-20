578933879a303061db75aa2bba47dc5a2be3ed27
aronweiler <aronweiler@gmail.com>
2023-07-20 13:36:45 -0700

    Added token count to llm chain

--- src/ai/llm_chain.py
+++ src/ai/llm_chain.py
@@ -5,6 +5,7 @@ from langchain.chains import LLMChain as llm_chain
 from langchain.memory import ConversationTokenBufferMemory
 
 from shared.selector import get_llm, get_chat_model
+from utilities.token_helper import num_tokens_from_string
 
 from ai.configurations.llm_chain_configuration import LLMChainConfiguration
 from ai.abstract_ai import AbstractAI
@@ -20,7 +21,7 @@ class LLMChain(AbstractAI):
         else:
             llm = get_llm(self.configuration.run_locally, local_model_path=self.configuration.model, ai_temp=float(self.configuration.ai_temp), max_tokens=-1)
 
-        memory = self._get_memory(llm, self.configuration.max_tokens) if self.configuration.use_memory else None                
+        memory = self._get_memory(llm) if self.configuration.use_memory else None                
         
         if self.configuration.prompt:
             if "inputs" in self.configuration.prompt:
@@ -32,12 +33,24 @@ class LLMChain(AbstractAI):
 
         self.chain = llm_chain(llm=llm, memory=memory, verbose=self.configuration.verbose, prompt=prompt)
 
-    def _get_memory(self, llm, max_tokens):
+    def _get_memory(self, llm):
         memory = ConversationTokenBufferMemory(llm=llm, memory_key="chat_history", return_messages=True, input_key="inputs", output_key="text")    
         
         return memory
      
     def query(self, input):
+
+        num_tokens = 0
+
+        if isinstance(input, str):
+            # If the input is a single string
+            num_tokens = num_tokens_from_string(input)
+        elif isinstance(input, list):
+            # If the input is a list of strings            
+            for string in input:
+                num_tokens += num_tokens_from_string(string)            
+
+        print(f"LLMChain query has {num_tokens} tokens")
 
         result = self.chain(inputs=input)
         

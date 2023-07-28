c8f54ad1b4b63ee18c956a00baf66dba14b7f5f9
aronweiler <aronweiler@gmail.com>
2023-07-28 10:34:35 -0700

    Added previous conversation context to prompt

--- src/runners/voice/prompts.py
+++ src/runners/voice/prompts.py
@@ -1,10 +1,14 @@
 VOICE_ASSISTANT_PROMPT = """
-My Current Time Zone: {time_zone}
-My Current Date / Time: {current_date_time}
+current_time_zone: {time_zone}
+current_date_time: {current_date_time}
+interaction_id: {interaction_id}
 
 Remember to make your responses phonetically correct, as well as spelling out any dates and times.
 
 Adjust your personality when responding to the user to be: {personality_keywords}
+
+Here is some context I found from previous conversations with this user:
+{context}
 
 Query from: {user_information}
 {query}
